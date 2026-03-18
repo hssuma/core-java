81046b6bf83cda7880451d82b422e3b2d5d24a5d
hssuma <sumahsiddappa@gmail.com>
2026-03-18 09:36:57 +0530

    Food Panda Executer

---
+++ methodOverloadingTwo/FoodPandaExec.java
@@ -0,0 +1,175 @@
+class FoodPandaExec
+{
+	public static void main(String ref[])
+	{
+		double price;
+
+		price = FoodPanda.search("burger", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("pizza", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("sandwich", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("french fries", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("pasta", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("noodles", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("fried rice", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("biryani", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("veg meals", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("paneer curry", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("chapati", 4);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("naan", 3);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("paratha", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("idli", 3);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("dosa", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("masala dosa", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("vada", 3);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("upma", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("pongal", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("poha", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("pav bhaji", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("samosa", 4);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("kachori", 3);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("chole bhature", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("rajma rice", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("dal rice", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("curd rice", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("lemon rice", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("tomato rice", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("veg pulao", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("momos", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("spring rolls", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("manchurian", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("chilli paneer", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("paneer tikka", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("veg cutlet", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("bread omelette", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("egg curry", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("chicken curry", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("fish fry", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("prawn curry", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("ice cream", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("chocolate cake", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("gulab jamun", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("rasgulla", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("jalebi", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("milkshake", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("cold coffee", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("tea", 3);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("coffee", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("green tea", 1);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("fruit juice", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("lassi", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("buttermilk", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("soft drink", 2);
+		System.out.println("The price is " + price);
+
+		price = FoodPanda.search("mineral water", 2);
+		System.out.println("The price is " + price);
+	}
+}
