c5e760fea2572a897ff637ece95d21e06f081f8c
hssuma <sumahsiddappa@gmail.com>
2026-03-18 09:33:33 +0530

    DoorDashExec

---
+++ methodOverloadingTwo/DoorDashExec.java
@@ -0,0 +1,172 @@
+class DoorDashExec
+{
+	public static void main(String ref[])
+	{
+		double price;
+
+		price = DoorDash.search("face wash", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face scrub", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face cream", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("moisturizer", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("sunscreen", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("toner", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("serum", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("night cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("day cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("under eye cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("lip balm", 3);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("lip scrub", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face mask", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("sheet mask", 3);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("body lotion", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("body butter", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("body scrub", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("hand cream", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("foot cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("cleansing milk", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("micellar water", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("makeup remover", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face oil", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("essential oil", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("aloe vera gel", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("rose water", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("charcoal mask", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("anti aging cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("acne gel", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("pimple patch", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("skin brightening cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("vitamin c serum", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("hyaluronic acid serum", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("niacinamide serum", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("retinol cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("spf lip balm", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("body oil", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("bath salts", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("bath oil", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face mist", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("cooling gel", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("tan removal cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("bb cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("cc cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("primer", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face gel", 2);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("eye gel", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("face peeling solution", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("lip mask", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("overnight mask", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("skin repair cream", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("glow serum", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("hydrating mask", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("detox mask", 1);
+		System.out.println("The price is " + price);
+
+		price = DoorDash.search("soothing cream", 1);
+		System.out.println("The price is " + price);
+	}
+}
