1d61f4356571f705aea5003e81583fd16e3b353d
hssuma <sumahsiddappa@gmail.com>
2026-03-18 09:44:01 +0530

    Magic Pin Executer

---
+++ methodOverloadingTwo/MagicPinExec.java
@@ -0,0 +1,181 @@
+class MagicPinExec
+{
+	public static void main(String ref[])
+	{
+		double price;
+
+		price = magicPin.search("gold ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver ring", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver necklace", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold chain", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver chain", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("platinum chain", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver earrings", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold bracelet", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver bracelet", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("platinum bracelet", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold bangle", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver bangle", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond bangle", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("anklet", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("toe ring", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("nose ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("nose pin", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("maang tikka", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("choker necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("pearl necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("pearl earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("pearl bracelet", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("emerald ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("ruby ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("sapphire ring", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold pendant", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond pendant", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver pendant", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("locket", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("cufflinks", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("brooch", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("hair pin jewelry", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("waist chain", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("temple jewelry set", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("bridal jewelry set", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("antique necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("oxidized earrings", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("oxidized necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("oxidized ring", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("beaded necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("beaded bracelet", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("stone earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("stone necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("gold studs", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("diamond studs", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("silver studs", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("jhumka earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("hoop earrings", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("drop earrings", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("layered necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("charm bracelet", 2);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("thread necklace", 1);
+		System.out.println("The price is " + price);
+
+		price = magicPin.search("custom name pendant", 1);
+		System.out.println("The price is " + price);
+	}
+}
