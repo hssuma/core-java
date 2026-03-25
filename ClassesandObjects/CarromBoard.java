5e15d34ef648d64774c7038d6bc8df9d1cf8bf52
hssuma <sumahsiddappa@gmail.com>
2026-03-25 12:28:27 +0530

    Carrom board

---
+++ ClassesandObjects/CarromBoard.java
@@ -0,0 +1,15 @@
+class CarromBoard
+{
+	int boardId ;
+	String brandName;
+	double dimension;
+	String color ;
+	String countryOfOrigin;
+	int norOfCoin ;
+	int norOfWhiteCoin ;
+	int norOfBlackCoin ;
+	String ASIN ;
+	int weight ;
+	String theme;
+	int cost;
+}
